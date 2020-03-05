e5dfa5e56222b6ca5ade024daccd46416fa337f6
Tom Spencer <tom@homeflow.co.uk>
2020-03-05 14:45:33 +0000

    Add addCell and getCell for Board

--- src/main/java/com/codurance/game_of_life/Board.java
+++ src/main/java/com/codurance/game_of_life/Board.java
@@ -4,17 +4,13 @@ import java.util.HashMap;
 
 public class Board {
 
-  private final HashMap cells;
+  private final HashMap<String, Cell> cells;
 
   public Board() {
     this.cells = new HashMap();
   }
 
-//  public int[][] cells() {
-//    return cells;
-//  }
-
-  public Object getCell(int i, int i1) {
+  public Cell getCell(int i, int i1) {
     return cells.get("1:1");
   }
 
--- src/test/java/com/codurance/game_of_life/BoardShould.java
+++ src/test/java/com/codurance/game_of_life/BoardShould.java
@@ -1,5 +1,6 @@
 package com.codurance.game_of_life;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -8,9 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardShould {
+  private Board board;
+  @BeforeEach
+  void setUp() {
+    board = new Board();
+  }
+
   @Test
   void have_array_of_empty_cells() {
-    Board board = new Board();
     Cell cell = new Cell(true);
     board.addCell(cell);
 
