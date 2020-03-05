4a10a20fa66cfb2c9d2a9975124d215200248ba0
Tom Spencer <tom@homeflow.co.uk>
2020-03-05 13:15:38 +0000

    Refactor GameOfLife to use Board for the logic

--- src/main/java/com/codurance/game_of_life/Board.java
+++ src/main/java/com/codurance/game_of_life/Board.java
@@ -21,6 +21,36 @@ public class Board {
     return new Board(board);
   }
 
+  public Board nextGeneration() {
+    int livingNeighbours = getNeighbours(1, 1);
+
+    board[1][1] = board[1][1] == 1 ? tickLivingCell(livingNeighbours) : tickDeadCell(livingNeighbours);
+
+    return this;
+  }
+
+  private int getNeighbours(int x, int y) {
+    int neighbours = 0;
+    for (int i = x - 1; i <= x + 1; i++) {
+      for (int j = y - 1; j <= y + 1; j++) {
+        if (i == x && j == y){
+          continue;
+        }
+        neighbours += board[i][j];
+      }
+    }
+    return neighbours;
+  }
+
+
+  private int tickDeadCell(int livingNeighbours) {
+    return livingNeighbours == 3 ? 1 : 0;
+  }
+
+  private int tickLivingCell(int livingNeighbours) {
+    return livingNeighbours >= 2 && livingNeighbours <= 3 ? 1 : 0;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
--- src/main/java/com/codurance/game_of_life/GameOfLife.java
+++ src/main/java/com/codurance/game_of_life/GameOfLife.java
@@ -9,47 +9,7 @@ public class GameOfLife {
   }
 
   public Board nextGen() {
-    int[][] nextGenArray = nextArray();
-    return board = new Board(nextGenArray);
-
-//    return board = nextBoard();
-  }
-
-//  private Board nextBoard() {
-//    Board nextBoard = board.createClone();
-//    return nextBoard.
-//    int nextCellState = nextBoard.getCellState(1,1) == 1 ? tickLivingCell(livingNeighbours) : tickDeadCell(livingNeighbours);
-//    nextCellState == 1 ? nextBoard.cellAlive(1,1) : nextBoard.cellDead(1,1);
-//  }
-
-  private int[][] nextArray(){
-    int[][] nextArray = board.getBoard();
-    int livingNeighbours = getNeighbours(1, 1);
-
-    nextArray[1][1] = board.getCellState(1,1) == 1 ? tickLivingCell(livingNeighbours) : tickDeadCell(livingNeighbours);
-
-    return nextArray;
-  }
-
-  private int tickDeadCell(int livingNeighbours) {
-    return livingNeighbours == 3 ? 1 : 0;
-  }
-
-  private int tickLivingCell(int livingNeighbours) {
-    return livingNeighbours >= 2 && livingNeighbours <= 3 ? 1 : 0;
-  }
-
-  private int getNeighbours(int x, int y) {
-    int neighbours = 0;
-    for (int i = x - 1; i <= x + 1; i++) {
-      for (int j = y - 1; j <= y + 1; j++) {
-        if (i == x && j == y){
-          continue;
-        }
-        neighbours += board.getCellState(i, j);
-      }
-    }
-    return neighbours;
+    return board = board.nextGeneration();
   }
 }
 
