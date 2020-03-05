b9a5f4974b05c207286966c098c627298d9974fd
Ewan <ewan.s@cucumberwifi.io>
2020-03-05 16:16:05 +0000

    failing test for coordinate not next to another

--- src/test/java/com/codurance/game_of_life/CoordinateShould.java
+++ src/test/java/com/codurance/game_of_life/CoordinateShould.java
@@ -2,6 +2,7 @@ package com.codurance.game_of_life;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CoordinateShould {
@@ -11,5 +12,13 @@ public class CoordinateShould {
     Coordinate coordinate1 = new Coordinate(1, 2);
 
     assertTrue(coordinate.isNextTo(coordinate1));
+  }
+
+  @Test
+  void return_false_if_not_next_to_other_coordinate() {
+    Coordinate coordinate = new Coordinate(1, 1);
+    Coordinate coordinate1 = new Coordinate(1, 3);
+
+    assertFalse(coordinate.isNextTo(coordinate1));
   }
 }
